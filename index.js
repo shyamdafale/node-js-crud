c31ee458f9e37c63fdcd840fa6b320d81270c9e3
abhilash <abhilash.kamble376@gmail.com>
2021-09-03 20:06:12 +0530

    netlify-server addedd

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const url = "mongodb://localhost/UrbanFarmerDB";
+// const url = "mongodb://localhost/UrbanFarmerDB";
+const url = `mongodb+srv://admin:admin@cluster0.kszi7.mongodb.net/urbanfarmer?retryWrites=true&w=majority`
 const app = express();
 const morgan = require('morgan')
 app.use(morgan('combined'))
+const serverless = require("serverless-http");
+
 
 mongoose.connect(url, { useNewUrlParser: true });
 const con = mongoose.connection;
@@ -24,3 +27,7 @@ app.use('/users', userRouter);
 app.listen(4000, () => {
     console.log("The Server is running on http://localhost:9000");
 });
+
+
+
+module.exports.handler = serverless(app);
